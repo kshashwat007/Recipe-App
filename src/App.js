cf4eabb9d565fcc7dc939a4e0f4e830c0232f557
kshashwat007 <kshashwat007@gmail.com>
2020-04-26 13:19:28 +0400

    changes made and api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import Form from './components/form';
 
-function App() {
-  return (
+
+const App = () => {
+  
+  const APP_ID = 'c7dad31d';
+  const API_KEY = 'e8a676191da6bfe0d774109b5d4e509f';
+
+  useEffect(() => {
+    getRecipes();
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  return(
     <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Recipes</h1>
-        </header>
-        <Form />
+          <header className="App-header">
+            <h1 className="App-title">Recipes</h1>
+          </header>
+          <form className='search-form'>
+        <input className='search-bar' type="text"/>
+        <button className='search-button' type='submit'>
+          Search
+        </button>
+    </form>
       </div>
   );
-}
+};
 
 export default App;
