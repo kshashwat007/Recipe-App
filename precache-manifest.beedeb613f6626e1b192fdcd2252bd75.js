10629ee05ffdf919037bce6207e1fa049c2470bb
kshashwat007 <kshashwat007@gmail.com>
2020-05-11 20:51:47 +0400

    Updates

---
+++ precache-manifest.beedeb613f6626e1b192fdcd2252bd75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a008592d3a6d67ce3b73537c1432172",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "35ae0ae54ce3a108fdfd",
+    "url": "/Recipe-App/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fb7e8f5cad0747bc81e5",
+    "url": "/Recipe-App/static/css/main.4bc0475d.chunk.css"
+  },
+  {
+    "revision": "35ae0ae54ce3a108fdfd",
+    "url": "/Recipe-App/static/js/2.e352b3d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.e352b3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7e8f5cad0747bc81e5",
+    "url": "/Recipe-App/static/js/main.519bc99d.chunk.js"
+  },
+  {
+    "revision": "2d74fd8561abb68c5dee",
+    "url": "/Recipe-App/static/js/runtime-main.7b68349c.js"
+  }
+]);
